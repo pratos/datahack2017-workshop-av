a31e483370763ee43a5806f0cc7f9d0fefe93bca
pratos <pratham.sarang9@gmail.com>
2017-11-04 19:47:01 +0530

    Cleaning the slides & hack session code notebook

--- hello-world-flask/final_demo/hello-world.py
+++ hello-world-flask/final_demo/hello-world.py
@@ -1,18 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-	return("Hello, World")
+    return ("Hello, World")
 
 
-# @app.route('/minion/<string:username>')
-# def offerings(username=None):
-# 	return("Welcome {}".format(username))
-
+@app.route('/minion/<string:username>')
+def another_function(username=None):
+    return("Welcome {}".format(username))
 
 if __name__ == '__main__':
-	app.run(port=5000)
+    app.run(port=5000)
